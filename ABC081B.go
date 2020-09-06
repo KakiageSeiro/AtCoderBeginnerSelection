c6a58d7e5dcc25f64b74c0e132f3e456adb32aa4
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2020-09-07 02:14:38 +0900

    ABC081B

---
+++ ABC081B.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ###
+// https://atcoder.jp/contests/abs/tasks/abc081_b
+// ###
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	n, a := nextLine(), nextLine()
+
+	intslice := createIntSlice(a, n)
+
+	// 割り算を実行した回数
+	divisionCount, _ := division(intslice, 0) 
+
+	fmt.Printf("%d", divisionCount)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func createIntSlice(a string, n string) []int {
+	var intslice []int
+	for _, s := range strings.Split(a, " ") {
+		atoi, err := strconv.Atoi(s)
+		if err != nil {
+			panic("Aに整数以外の値が入力されました")
+		}
+
+		intslice = append(intslice, atoi)
+	}
+
+	atoi, _ := strconv.Atoi(n)
+	if atoi != len(intslice) {
+		panic("Nの値と、Aの個数が違います")
+	}
+	return intslice
+}
+
+func division(intSlice []int, divisionCount int) (int, []int) {
+	var harfSlice []int //割り算実行後の値を保持するスライス
+	for _, i2 := range intSlice {
+		if i2%2 != 0 {
+			// 奇数が一つでもある場合は処理終了
+			return divisionCount, intSlice
+		}
+
+		harfSlice = append(harfSlice, i2/2)
+	}
+
+	return division(harfSlice, divisionCount+1)
+}
