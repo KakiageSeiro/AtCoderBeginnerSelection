d9e4ef75dcdaa8e89ac65b8baf93f5bc8206c048
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2020-09-10 03:21:12 +0900

    標準入力からの受付方法をscanに変更したら通った

--- ABC087B.go
+++ ABC087B.go
@@ -1,86 +1,40 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 // ###
 // https://atcoder.jp/contests/abs/tasks/abc087_b
 // ###
 
-// 標準入力から一行読み取る
-func nextLine_abc087_b() string {
-	var sc = bufio.NewScanner(os.Stdin)
-	sc.Scan()
-	return sc.Text()
-}
-
 func main() {
-	a, b, c, x := nextLine_abc087_b(), nextLine_abc087_b(), nextLine_abc087_b(), nextLine_abc087_b()
-	coinCount := cast(a, b, c)
-	xYen, _ := strconv.Atoi(x)
 
-	chooses := createChooseSlice(coinCount)
+	var a, b, c, x int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	fmt.Scanf("%d", &c)
+	fmt.Scanf("%d", &x)
 
-	// 指定された合計金額と一致するコインの組み合わせ数
-	jastCount := 0
-	for _, ch := range chooses {
-		sum := (ch.count500 * 500) + (ch.count100 * 100) + (ch.count50 * 50)
-
-		if sum == xYen {
-			jastCount++
-		}
-	}
+	jastCount := createChooseSlice(a, b, c, x)
 
 	fmt.Print(jastCount)
 }
 
 // コイン選択方法の組み合わせ
-func createChooseSlice(coinCount coinCount) []choose {
-	chooses := []choose{}
-	for i := 0; i < coinCount.a500+1; i++ { // 500のコインが0枚の場合でも、0枚を選択する組み合わせを実行したいため、条件式に+1を指定(iが0のときはループを実行される)
-		currentChoose := choose{}
-		currentChoose.count500 = i
+func createChooseSlice(a, b, c, xYen int) int {
+	jastCount := 0
+	for i := 0; i < a+1; i++ { // 500のコインが0枚の場合でも、0枚を選択する組み合わせを実行したいため、条件式に+1を指定(iが0のときはループを実行される)
+		for i2 := 0; i2 < b+1; i2++ {
+			for i3 := 0; i3 < c+1; i3++ {
+				sum := (i * 500) + (i2 * 100) + (i3 * 50)
 
-		for i := 0; i < coinCount.b100+1; i++ {
-			currentChoose.count100 = i
-
-			for i := 0; i < coinCount.c50+1; i++ {
-				currentChoose.count50 = i
-
-				chooses = append(chooses, currentChoose)
+				if sum == xYen {
+					jastCount++
+				}
 			}
 		}
 	}
 
-	return chooses
-}
-
-func cast(a, b, c string) coinCount {
-	a500, _ := strconv.Atoi(a)
-	b100, _ := strconv.Atoi(b)
-	c50, _ := strconv.Atoi(c)
-
-	return coinCount{
-		a500: a500,
-		b100: b100,
-		c50:  c50,
-	}
-}
-
-// 入力されたコインの枚数
-type coinCount struct {
-	a500 int
-	b100 int
-	c50  int
-}
-
-// 各コインの選び方の組み合わせ
-type choose struct {
-	count500 int
-	count100 int
-	count50  int
+	return jastCount
 }
